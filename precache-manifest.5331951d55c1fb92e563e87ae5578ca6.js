2f65f5c8d8ef071eb0fb3fb160fdba5e87a2f344
Dilin Dampahalage <dilin.dampahalage@gmail.com>
2019-06-04 18:14:07 +0530

    Updates

---
+++ precache-manifest.5331951d55c1fb92e563e87ae5578ca6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef4a6dc51ee9eb6c7e2feb4daed0407",
+    "url": "/dilin993/index.html"
+  },
+  {
+    "revision": "398810252fa813ce729b",
+    "url": "/dilin993/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/dilin993/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "398810252fa813ce729b",
+    "url": "/dilin993/static/js/main.cc705fa4.chunk.js"
+  },
+  {
+    "revision": "2577a8eed4cae2e7f049",
+    "url": "/dilin993/static/js/runtime~main.6ef28f8c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dilin993/static/media/logo.5d5d9eef.svg"
+  }
+]);
